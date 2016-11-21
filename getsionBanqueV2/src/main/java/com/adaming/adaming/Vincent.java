46e41a620d1b4f1755314a1e8c2f1e1111f22b51
inti0310 <inti0310@DESKTOP-LIA9SER>
2016-11-21 15:08:35 +0100

    Signed-off-by: inti0310 <inti0310@DESKTOP-LIA9SER>

---
+++ getsionBanqueV2/src/main/java/com/adaming/adaming/Vincent.java
@@ -0,0 +1,8 @@
+package com.adaming.adaming;
+
+public class Vincent {
+	public static void main(String[] args) {
+		System.out.println("qui c'est le meilleur?");
+		System.out.println("qui c'est le meilleur?");
+	}
+}
